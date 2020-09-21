f13d30c17e7682663328fded4d5d25dca4b4f5dd
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-21 22:01:44 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,7 +57,7 @@ end
   
   def turn
     puts "Please enter a number 1-9:"
-    user_input = gets.chomp
+    user_input = gets.strip
     index = input_to_index(user_input)
     if valid_move?(index)
       token = current_player
