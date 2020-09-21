72eb78170bc37df139618dbab3007c2ceff6e806
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-21 21:34:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,7 +60,7 @@ end
     user_input = gets.strip
     index = input_to_index(user_input)
     if valid_move?(index)
-      move(index, player_token)
+      move(index, current_player)
       display_board
     else
       turn
