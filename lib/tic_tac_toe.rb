f16a8f6cb639d9a67c82029a8266021596a58c37
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-21 21:41:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,14 +77,25 @@ end
   end 
   
   def over?
+    won? || draw?
   end 
   
   def winner
+    if winning_combo= won?
+      @board[winning_combo.first]
+    end
   end 
   
   def play
+    while !over?
+      turn
+    end
+    if won?
+      put "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end
   end
-  
   
 end
 
